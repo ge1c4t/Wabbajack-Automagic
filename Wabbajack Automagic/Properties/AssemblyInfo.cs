270e67e44bee67b4a96a3bdf122bca286a278e91
Emily <73432364+ge1c4t@users.noreply.github.com>
2021-08-20 13:11:58 -0500

    Trying to fix anti virus false positive

--- Wabbajack Automagic/Properties/AssemblyInfo.cs
+++ Wabbajack Automagic/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Wabbajack Automagic")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An autoclicker for Wabbajack modlist installer.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ge1c4t")]
 [assembly: AssemblyProduct("Wabbajack Automagic")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1")]
+[assembly: AssemblyFileVersion("1.1")]
